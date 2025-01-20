f3d7a72d252b600e36f83b7c4e5dfa4366626fef
Jackachulian <jackachulian@gmail.com>
2025-01-19 21:25:31 -0600

    Purple outline around nearby possessable objects

--- project_purgatory/Scripts/possession.gd
+++ project_purgatory/Scripts/possession.gd
@@ -2,13 +2,16 @@
 extends Node2D
 
 # Objects the player has added to their transformable objects list
-var transformable_objects: Array[Node] = []
+var possessable_inventory: Array[Node] = []
 
 @onready var possession_radius: Area2D = $PossessionRadius
 var possessable_objects_in_radius: Array[Node] = []
 
 # Possessable object within the possesion radius closest to the player. Updated each frame.
 var closest_possessable: Node
+
+# Material to apply to the sprite of the nearest possessable object.
+var possessable_material: Material
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -38,6 +41,12 @@ func update_closest_possessable():
 func _on_possession_radius_body_entered(body: Node2D) -> void:
 	if body.is_in_group("possessable"):
 		possessable_objects_in_radius.append(body);
+		var sprite: Sprite2D = body.get_node("Sprite2D")
+		if sprite and sprite.material:
+			sprite.material.set_shader_parameter("width", 1.0)
 	
 func _on_possession_radius_body_exited(body: Node2D) -> void:
 	possessable_objects_in_radius.erase(body);
+	var sprite: Sprite2D = body.get_node("Sprite2D")
+	if sprite and sprite.material:
+		sprite.material.set_shader_parameter("width", 0.0)
