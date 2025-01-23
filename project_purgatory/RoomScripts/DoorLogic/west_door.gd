45c495b6ec474d4cc83c27ffdcb08d5cdd50897d
zakattack157 <zhickey2001@gmail.com>
2025-01-23 07:45:53 -0600

    New Rooms and Camera System
    Added a new large room. Reworked camera system to be based on rooms themselves. Added functionality for moving camera in large rooms

---
+++ project_purgatory/RoomScripts/DoorLogic/west_door.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		body.position += Vector2(-150, 0)
+		#var camera = body.get_node("Camera2D")
+		#camera.set_position_smoothing_enabled(true)
+		#body.can_move = false
+		#await get_tree().create_timer(1.5).timeout
+		#
+		#camera.set_position_smoothing_enabled(false)
+		#body.can_move = true
+
+		
+
+		
