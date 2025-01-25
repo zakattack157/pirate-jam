5a3e22699dc4e431b07cc0652e66b21b09fe84a3
zakattack157 <zhickey2001@gmail.com>
2025-01-25 01:47:54 -0600

    Real player model implemented
    Addition of the actual player model and its animations.

--- project_purgatory/TestPlayer/PlayerMovement.gd
+++ project_purgatory/TestPlayer/PlayerMovement.gd
@@ -1,11 +1,28 @@
 extends CharacterBody2D
 
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var speed = 400
 var can_move: bool = true # True, player can move. False, player does not move
 
+func _ready():
+	anim.play("Idle")
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+	
+	match input_direction:
+		Vector2(-1,0):
+			anim.play("Walk_Left")
+		Vector2(1,0):
+			anim.play("Walk_Right")
+		Vector2(0,-1):
+			anim.play("Walk_Up")
+		Vector2(0,1):
+			anim.play("Idle")
+		Vector2(0,0):
+			anim.play("Idle")
+	
 	velocity = input_direction * speed
 
 func _physics_process(delta):
