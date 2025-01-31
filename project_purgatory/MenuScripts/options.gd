b866d0d7b86303ef8bd79590f785694617b4313c
zakattack157 <zhickey2001@gmail.com>
2025-01-30 23:33:29 -0600

    menus and retiling
    added main menu, options menu, and quit functionality. Reskinned dungeon

---
+++ project_purgatory/MenuScripts/options.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_back_to_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menus/MainMenu.tscn")
